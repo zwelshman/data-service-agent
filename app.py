215b8260e901a587043caa2fac1d37623bbd3bcf
Zach Welshman <38403427+zwelshman@users.noreply.github.com>
2026-04-09 11:39:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ VAULT_ID = st.secrets["VAULT_ID"]
 BETAS = ["managed-agents-2026-04-01"]
 
 st.title("BHF DSC Assistant")
-st.markdown("""Use natural languate to search \n 
-            [bhf hds docs](https://bhfdsc.github.io/documentation/) \n 
-            [bhf standard pipeline](https://github.com/BHFDSC/standard-pipeline/) \n 
-            [bhf data summary dashboard](https://bhfdatasciencecentre.org/dashboard/)
-           """)
-
+st.markdown("""
+### Quick Links
+Use natural language to search:
+* [BHF HDS Docs](https://bhfdsc.github.io/documentation/)
+* [BHF Standard Pipeline](https://github.com/BHFDSC/standard-pipeline/)
+* [BHF Data Summary Dashboard](https://bhfdatasciencecentre.org/dashboard/)
+""")
 # Create a session once per Streamlit session
 if "session_id" not in st.session_state:
     session = client.beta.sessions.create(
